51470226bc3dbb2433bd45a04e247a941f3e3ccc
Molebogeng Selahle <business.molebogeng@gmail.com>
2026-05-02 18:56:49 +0200

    Created a thread test the connection between server and user. Also for multiple connections

---
+++ Systems Integration/Socket_programming/Smart_Home_IoT_Hub/src/main/java/Server/HubWorkerThread.java
@@ -0,0 +1,36 @@
+package Server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class HubWorkerThread implements Runnable{
+    private final Socket server;
+    private static int userCount;
+
+    public HubWorkerThread(Socket server){
+        this.server = server;
+    }
+
+    @Override
+    public void run(){
+        try {
+            //writing from a user and writing to a user
+            PrintWriter write = new PrintWriter(server.getOutputStream());
+            Scanner read = new Scanner(server.getInputStream());
+
+            //Identifying the connected user and sending a response to user
+            userCount++;
+            System.out.println("User: "+userCount+"\nId: "+server.getPort()+"\nConnected!");
+            write.println("Successfully connected to server");
+            write.flush();
+
+
+            //while (true) {}
+        } catch (IOException e) {
+            System.out.println("Server Disconnected!");
+            System.exit(1);
+        }
+    }
+}
