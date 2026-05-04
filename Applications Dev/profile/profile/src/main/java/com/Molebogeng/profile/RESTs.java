7a3ce765f038b59152a05af667efd23dccc2b1c6
Molebogeng Selahle <business.molebogeng@gmail.com>
2026-05-04 14:18:42 +0200

    Created the rest Controller

--- Applications Dev/profile/profile/src/main/java/com/Molebogeng/profile/RESTs.java
+++ Applications Dev/profile/profile/src/main/java/com/Molebogeng/profile/RESTs.java
@@ -1,4 +1,23 @@
 package com.Molebogeng.profile;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RESTs {
+
+    //simple display of my index
+    @GetMapping("/index")
+    public String index(){
+        return "Explore Molebogeng Lehlogonolo Selahle";
+    }
+
+    //simple contact me form
+    @PostMapping("/contactMe")
+    public String contactMe(@RequestBody ContactMe contactMe){
+
+        return "Form successfully loaded";
+    }
 }
