d60c40a6f92634bf6c4ef45283b86df393f56b02
Molebogeng Selahle <business.molebogeng@gmail.com>
2026-06-06 01:49:06 +0200

    changed to google maps

--- Applications Dev/KoKo/src/main/resources/static/js/map.js
+++ Applications Dev/KoKo/src/main/resources/static/js/map.js
@@ -1,41 +1,66 @@
 // === KOKO MAP ===
 
 let map;
-let userMarker;
 
 function initMap() {
-  // Default location
-  map = L.map('map').setView([-26.1550, 28.3840], 12);
 
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    attribution: '© OpenStreetMap © CARTO',
-    subdomains: 'abcd',
-    maxZoom: 20}).addTo(map);
+  const daveyton = { lat: -26.1550, lng: 28.3840 };
 
-  map.on('click', function(e) {
-    const { lat, lng } = e.latlng;
-
-    if (userMarker) {
-      map.removeLayer(userMarker);
-    }
-
-    userMarker = L.marker([lat, lng]).addTo(map)
-      .bindPopup('Your location').openPopup();
-
-    loadBusinesses(lat, lng);
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: daveyton,
   });
 
-  // Try get user's real location
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      const lat = pos.coords.latitude;
-      const lng = pos.coords.longitude;
-      map.setView([lat, lng], 12);
-      userMarker = L.marker([lat, lng]).addTo(map)
-        .bindPopup('You are here').openPopup();
-      loadBusinesses(lat, lng);
-    });
-  }
+  //marker for Daveyton
+  new google.maps.Marker({
+    position: daveyton,
+    map: map,
+    title: "Daveyton"
+  });
+
+  //PLACES SERVICE (THIS SHOWS REAL BUSINESSES)
+  const service = new google.maps.places.PlacesService(map);
+
+  service.nearbySearch(
+    {
+      location: daveyton,
+      radius: 2000,   // 2km around Daveyton
+      type: ["store"] // Will change this later from POST (selecting from catagories)
+    },
+    (results, status) => {
+
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+
+        results.forEach(place => {
+
+          new google.maps.Marker({
+            position: place.geometry.location,
+            map: map,
+            title: place.name
+          });
+
+        });
+
+      }
+    }
+  );
+
+  // Pin design
+  const marker = new google.maps.Marker({
+  position: place.geometry.location,
+  map: map,
+  title: place.name
+});
+
+const info = new google.maps.InfoWindow({
+  content: `<strong>${place.name}</strong>`
+});
+
+marker.addListener("click", () => {
+  info.open(map, marker);
+});
 }
+
+
 
 window.onload = initMap;
