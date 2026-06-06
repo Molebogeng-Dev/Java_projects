e60b7e36694e57100ede34b67aba1f9f17e81851
Molebogeng Selahle <business.molebogeng@gmail.com>
2026-06-06 15:35:42 +0200

    Moved KoKo to its own repo

--- Applications Dev/KoKo/src/main/java/co/za/KoKo/webApp/Rests/backendDbClass/apiRegisterDbRepo.java
+++
@@ -1,6 +0,0 @@
-package co.za.KoKo.webApp.Rests.backendDbClass;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface apiRegisterDbRepo extends JpaRepository<apiRegisterDb, Long> {
-}
